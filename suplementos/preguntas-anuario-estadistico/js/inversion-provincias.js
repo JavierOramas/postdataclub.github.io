e3f5f64b151fb64a294f38e2d781dbdcef6f3088
JavierOramas <javiale2000@gmail.com>
2022-05-26 22:15:15 -0400

    charts for inversion working OK

--- suplementos/preguntas-anuario-estadistico/js/inversion-provincias.js
+++ suplementos/preguntas-anuario-estadistico/js/inversion-provincias.js
@@ -1,162 +1,180 @@
-myChart.showLoading();
-$.get(ROOT_PATH + '/data/inversion-provincias.json', function (data) {
-  var inversionDiv = document.getElementById('inversion-chart');
-  var myChart = echarts.init(inversionDiv, "dark");
-    
-  myChart.hideLoading();
-  var itemStyle = {
-    opacity: 0.8
-  };
-  var sizeFunction = function (x) {
-    var y = Math.sqrt(x / 5e8) + 0.1;
-    return y * 80;
-  };
-  // Schema:
-  var schema = [
-    { name: 'Income', index: 0, text: '人均收入', unit: '美元' },
-    { name: 'LifeExpectancy', index: 1, text: '人均寿命', unit: '岁' },
-    { name: 'Population', index: 2, text: '总人口', unit: '' },
-    { name: 'Country', index: 3, text: '国家', unit: '' }
-  ];
-  option = {
-    baseOption: {
-      timeline: {
-        axisType: 'category',
-        orient: 'vertical',
-        autoPlay: true,
-        inverse: true,
-        playInterval: 1000,
-        left: null,
-        right: 0,
-        top: 20,
-        bottom: 20,
-        width: 55,
-        height: null,
-        symbol: 'none',
-        checkpointStyle: {
-          borderWidth: 2
-        },
-        controlStyle: {
-          showNextBtn: false,
-          showPrevBtn: false
-        },
-        data: []
-      },
-      title: [
-        {
-          text: data.timeline[0],
-          textAlign: 'center',
-          left: '63%',
-          top: '55%',
-          textStyle: {
-            fontSize: 100
-          }
+$.getJSON('data/inversion.json', function (data) {
+    var inversionDiv = document.getElementById('inversion-i-chart');
+    var myChart = echarts.init(inversionDiv);
+
+    myChart.hideLoading();
+    var itemStyle = {
+        opacity: 0.8
+    };
+    var sizeFunction = function (x) {
+        return Math.sqrt(x);
+    };
+    // Schema:
+    var schema = [{
+            name: 'Inversion',
+            index: 0,
+            text: 'Inversión',
+            unit: 'Millones de pesos',
         },
         {
-          text: 'Inversión por Densidad poblacional',
-          left: 'center',
-          top: 10,
-          textStyle: {
-            fontWeight: 'normal',
-            fontSize: 20
-          }
-        }
-      ],
-      tooltip: {
-        padding: 5,
-        borderWidth: 1,
-        formatter: function (obj) {
-          var value = obj.value;
-          // prettier-ignore
-          return schema[3].text + '：' + value[3] + '<br>'
-                        + schema[1].text + '：' + value[1] + schema[1].unit + '<br>'
-                        + schema[0].text + '：' + value[0] + schema[0].unit + '<br>'
-                        + schema[2].text + '：' + value[2] + '<br>';
-        }
-      },
-      grid: {
-        top: 100,
-        containLabel: true,
-        left: 30,
-        right: '110'
-      },
-      xAxis: {
-        type: 'log',
-        name: 'población',
-        max: 100000,
-        min: 300,
-        nameGap: 25,
-        nameLocation: 'middle',
-        nameTextStyle: {
-          fontSize: 18
+            name: 'Poblacion',
+            index: 1,
+            text: 'Población',
+            unit: 'Millones de personas',
         },
-        splitLine: {
-          show: false
-        },
-        axisLabel: {
-          formatter: '{value} M'
-        }
-      },
-      yAxis: {
-        type: 'value',
-        name: 'Inversión',
-        max: 100,
-        nameTextStyle: {
-          fontSize: 18
-        },
-        splitLine: {
-          show: false
-        },
-        axisLabel: {
-          formatter: '{value} M'
-        }
-      },
-      visualMap: [
+
         {
-          show: false,
-          dimension: 3,
-          categories: data.counties,
-          inRange: {
-            color: (function () {
-              // prettier-ignore
-              var colors = ['#51689b', '#ce5c5c', '#fbc357', '#8fbf8f', '#659d84', '#fb8e6a', '#c77288', '#786090', '#91c4c5', '#6890ba'];
-              return colors.concat(colors);
-            })()
-          }
+            name: 'Country',
+            index: 3,
+            text: 'Provincia',
+            unit: ''
         }
-      ],
-      series: [
-        {
-          type: 'scatter',
-          itemStyle: itemStyle,
-          data: data.series[0],
-          symbolSize: function (val) {
-            return sizeFunction(val[2]);
-          }
-        }
-      ],
-      animationDurationUpdate: 1000,
-      animationEasingUpdate: 'quinticInOut'
-    },
-    options: []
-  };
-  for (var n = 0; n < data.timeline.length; n++) {
-    option.baseOption.timeline.data.push(data.timeline[n]);
-    option.options.push({
-      title: {
-        show: true,
-        text: data.timeline[n] + ''
-      },
-      series: {
-        name: data.timeline[n],
-        type: 'scatter',
-        itemStyle: itemStyle,
-        data: data.series[n],
-        symbolSize: function (val) {
-          return sizeFunction(val[2]);
-        }
-      }
-    });
-  }
-  myChart.setOption(option);
+    ];
+    option = {
+        baseOption: {
+            timeline: {
+                axisType: 'category',
+                orient: 'vertical',
+                autoPlay: true,
+                inverse: true,
+                playInterval: 3000,
+                left: null,
+                right: 0,
+                top: 20,
+                bottom: 20,
+                width: 55,
+                height: null,
+                symbol: 'none',
+                checkpointStyle: {
+                    borderWidth: 2
+                },
+                controlStyle: {
+                    showNextBtn: false,
+                    showPrevBtn: false
+                },
+                data: []
+            },
+            title: [{
+                    text: data.timeline[0],
+                    textAlign: 'center',
+                    left: '63%',
+                    top: '55%',
+                    textStyle: {
+                        fontSize: 100
+                    }
+                },
+                {
+                    text: 'Inversión por Provincia',
+                    left: 'center',
+                    top: 10,
+                    textStyle: {
+                        fontWeight: 'normal',
+                        fontSize: 20
+                    }
+                }
+            ],
+            tooltip: {
+                padding: 5,
+                borderWidth: 1,
+                formatter: function (obj) {
+                    var value = obj.value;
+                    // prettier-ignore
+                    return schema[2].text + '：' + value[2] + '<br>' +
+                        schema[1].text + '：' + value[1] + " " + schema[1].unit + '<br>' +
+                        schema[0].text + '：' + value[0] + " " + schema[0].unit + '<br>';
+                    // schema[2].text + '：' + value[2] + '<br>';
+                }
+            },
+            grid: {
+                top: 100,
+                containLabel: true,
+                left: 30,
+                right: '110'
+            },
+            xAxis: {
+                type: 'log',
+                name: 'Inversión',
+                max: 10000,
+                min: 50,
+                nameGap: 25,
+                nameLocation: 'middle',
+                nameTextStyle: {
+                    fontSize: 18
+                },
+                splitLine: {
+                    show: false
+                },
+                axisLabel: {
+                    formatter: '${value} M'
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: 'Población',
+                max: 3000000,
+                nameTextStyle: {
+                    fontSize: 18
+                },
+                splitLine: {
+                    show: false
+                },
+                axisLabel: {
+                    formatter: '{value}'
+                }
+            },
+            visualMap: [{
+                show: false,
+                dimension: 2,
+                categories: data.counties,
+                inRange: {
+                    color: (function () {
+                        // prettier-ignore
+                        var colors = ['#51689b', '#ce5c5c', '#ce5', '#5c5c', '#fbc357', '#bce357', '#ac3337', , '#8fbf8f', '#659d84', '#fb8e6a', '#c77288', '#786090', '#91c4c5', '#6890ba'];
+                        return colors.concat(colors);
+                    })()
+                }
+            }],
+            series: [{
+                type: 'scatter',
+                itemStyle: itemStyle,
+                data: data.series[0],
+                symbolSize: function (val) {
+                    return sizeFunction(val[0]);
+                }
+            }],
+            animationDurationUpdate: 1000,
+            animationEasingUpdate: 'quinticInOut'
+        },
+        options: []
+    };
+    for (var n = 0; n < data.timeline.length; n++) {
+        option.baseOption.timeline.data.push(data.timeline[n]);
+        option.options.push({
+            title: {
+                show: true,
+                text: data.timeline[n] + ''
+            },
+            series: {
+                name: data.timeline[n],
+                type: 'scatter',
+                itemStyle: itemStyle,
+                data: data.series[n],
+                symbolSize: function (val) {
+                    return sizeFunction(val[0]);
+                }
+            }
+        });
+    }
+    myChart.setOption(option);
+
+    $(window).on('resize', resize);
+
+    // Resize function
+    function resize() {
+        setTimeout(function () {
+            // Resize chart
+            myChart.resize();
+        }, 200);
+    }
+
 });
